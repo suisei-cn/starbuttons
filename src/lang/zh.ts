f76b52b7f9fcec710007b73b94ef9c79b8c9dff8
Outvi V <git@outv.im>
2020-05-08 01:34:58 +0800

    fix(lang/zh): Telegram -> “观星台”

--- src/lang/zh.ts
+++ src/lang/zh.ts
@@ -1,7 +1,7 @@
 export default {
   "Do Not Click Me": "没事不要点",
   GitHub: "代码猫",
-  Telegram: "小飞机",
+  Telegram: "观星台",
   "Toggle chorus mode": "多重吟唱模式开关",
   "Music board": "声音面板",
   "Back": "返回"

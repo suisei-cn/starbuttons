7e05d93bce959c998e4513a913843eb02e57b9b6
Outvi V <git@outv.im>
2020-05-13 15:21:22 +0800

    fix(service-worker): Only precache ehhh{,2,3}

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,17 @@
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+const INITIAL_PRECACHING_URL = [
+  "/assets/ehhh.mp3",
+  "/assets/ehhh2.mp3",
+  "/assets/ehhh3.mp3",
+];
+
+/**
+ * @schema { revision, url }
+ */
+workbox.precaching.precacheAndRoute(
+  self.__precacheManifest.filter(obj => {
+    INITIAL_PRECACHING_URL.includes(obj.url);
+  }) || []
+);
 
 // workbox.routing.registerRoute(
 //   /\.(?:mp3|ogg|webm|m4a)$/,

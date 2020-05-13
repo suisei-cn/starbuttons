4c2d523c25e664939a8162446b87d8e5e0278379
Outvi V <git@outv.im>
2020-05-13 13:37:24 +0800

    feat(sw): Preload sound via Service Workers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from './store';
-import { i18n } from './i18n-setup';
+import store from "./store";
+import { i18n } from "./i18n-setup";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,18 @@ module.exports = {
       args[0].title = "Starbuttons";
       return args;
     });
+  },
+  pwa: {
+    themeColor: "#6375BC",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "src/service-worker.js",
+      importWorkboxFrom: "disabled",
+      importScripts:
+        "https://cdn.jsdelivr.net/npm/workbox-sw@5.1.3/build/workbox-sw.min.js"
+    }
   }
 };

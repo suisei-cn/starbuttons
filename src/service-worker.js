617eaf4b00405c1a0abda1e757428e9e3d7e3943
Outvi V <19144373+outloudvi@users.noreply.github.com>
2020-05-16 01:38:42 +0800

    fix(service-worker): Should CacheFirst for sounds

--- src/service-worker.js
+++ src/service-worker.js
@@ -13,15 +13,15 @@ workbox.precaching.precacheAndRoute(
   }) || []
 );
 
-// workbox.routing.registerRoute(
-//   /\.(?:mp3|ogg|webm|m4a)$/,
-//   new workbox.strategies.CacheFirst({
-//     cacheName: "audios",
-//     plugins: [
-//       new workbox.expiration.ExpirationPlugin({
-//         maxEntries: 120,
-//         maxAgeSeconds: 30 * 24 * 60 * 60,
-//       }),
-//     ],
-//   })
-// );
+workbox.routing.registerRoute(
+  /\.(?:mp3|ogg|webm|m4a)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "audios",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 120,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);

a0d5c144bbfc6b567d1bb0dfc12bb61eb03881c6
Outvi V <git@outv.im>
2020-05-18 18:33:41 +0800

    lint:

--- src/components/setLanguage.ts
+++ src/components/setLanguage.ts
@@ -2,7 +2,8 @@ export function setLanguage(window: Window, navigator: Navigator, vue: Vue) {
   const lang =
     (
       window.location.search.match(/lang=([a-zA-Z-]+)/)?.[1] ||
-      (navigator as any).language ||
+      (navigator as Navigator).language ||
+      // @ts-ignore no-implicit-any
       (navigator as any).userLanguage
     ).split("-")[0] || "zh";
   vue.$i18n.locale = lang;

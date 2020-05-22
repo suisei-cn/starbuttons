3941028dba348c7f37d5e1b235e066483366a21b
Outvi V <git@outv.im>
2020-05-22 18:03:41 +0800

    feat: Goodbye FA, hello icomoon

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { i18n } from "./i18n-setup";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faGithub,
-  faDiscord,
-  faTelegram
-} from "@fortawesome/free-brands-svg-icons";
 
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
-
-library.add(faGithub);
-library.add(faDiscord);
-library.add(faTelegram);
-Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,

ebf367ed1189af13bbb9c5da12a28f69188febde
Outvi V <git@outv.im>
2020-06-15 16:20:54 +0800

    feat: Add Sentry

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import { i18n } from "./i18n-setup";
 import CentralPlayer from "./components/centralPlayer";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
 
 import "./registerServiceWorker";
 
@@ -11,6 +13,11 @@ Vue.prototype.$status = {
   playCount: 0,
   player: new CentralPlayer()
 };
+
+Sentry.init({
+  dsn: "https://571ceb23ed804c08bc4a59087b4ba079@o407498.ingest.sentry.io/5276537",
+  integrations: [new VueIntegration({ Vue, attachProps: true })],
+});
 
 Vue.config.productionTip = false;
 

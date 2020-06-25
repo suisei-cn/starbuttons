841a4f4c40f24d41ebd854b76670bdaaccd7b4e3
Outvi V <git@outv.im>
2020-06-26 01:35:14 +0800

    feat: Remove service worker caching

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,34 +1,7 @@
-workbox.setConfig({
-  modulePathCb(name, debug) {
-    const env = debug ? "dev" : "prod";
-    return `https://cdn.jsdelivr.net/npm/${name}@5.1.3/build/${name}.${env}.js`;
-  }
+self.addEventListener("install", function(event) {
+  event.waitUntil(self.skipWaiting());
 });
 
-const INITIAL_PRECACHING_URL = [
-  "/assets/ehhh.mp3",
-  "/assets/ehhh2.mp3",
-  "/assets/ehhh3.mp3"
-];
-
-/**
- * @schema { revision, url }
- */
-workbox.precaching.precacheAndRoute(
-  self.__precacheManifest.filter(obj => {
-    INITIAL_PRECACHING_URL.includes(obj.url);
-  }) || []
-);
-
-workbox.routing.registerRoute(
-  /\.(?:mp3|ogg|webm|m4a)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: "audios",
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 120,
-        maxAgeSeconds: 30 * 24 * 60 * 60
-      })
-    ]
-  })
-);
+self.addEventListener("activate", function(event) {
+  event.waitUntil(self.clients.claim());
+});

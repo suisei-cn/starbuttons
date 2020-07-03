f269859837a104cdaa2fcab2ca463cd9473e2e12
Outvi V <git@outv.im>
2020-07-03 23:45:00 +0800

    feat(utils/bitrate): Add -o for filename export

--- utils/bitrate.sh
+++ utils/bitrate.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+# Arguments:
+#   -o: Only output audio filenames
 TARGET_BITRATE=200
 
-pushd public/assets
+cd public/assets
 HAVE_HI_BIT=0
 for i in $(ls | grep -v README.md); do
     BITRATE=$(ffprobe $i 2>&1 | grep Duration | awk '{print $6}')
     if [ "$BITRATE" -gt "$TARGET_BITRATE" ]; then
-        echo "Too high bitrate for '$i' : $BITRATE kbps"
-        HAVE_HI_BIT=$((HAVE_HI_BIT + 1))
+        if [ "$1" != "-o" ]; then
+            echo "Too high bitrate for '$i' : $BITRATE kbps"
+            HAVE_HI_BIT=$((HAVE_HI_BIT + 1))
+        else
+            echo $i
+        fi
     fi
 done
-popd
+cd ../..
 exit $HAVE_HI_BIT

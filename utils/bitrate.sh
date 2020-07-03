fc04262355cc67bf78ddb47a9192024ea1880aa7
Outvi V <git@outv.im>
2020-07-03 23:32:18 +0800

    fix(utils); fix bitrate scripts

--- utils/bitrate.sh
+++ utils/bitrate.sh
@@ -4,7 +4,7 @@ TARGET_BITRATE=200
 pushd public/assets
 HAVE_HI_BIT=0
 for i in $(ls | grep -v README.md); do
-    BITRATE=$(ffprobe $i 2>&1 | grep bitrate | awk '{print $6}')
+    BITRATE=$(ffprobe $i 2>&1 | grep Duration | awk '{print $6}')
     if [ "$BITRATE" -gt "$TARGET_BITRATE" ]; then
         echo "Too high bitrate for '$i' : $BITRATE kbps"
         HAVE_HI_BIT=$((HAVE_HI_BIT + 1))

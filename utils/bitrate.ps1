af4119ddb97693ca46692cf9e13c470302f45788
Hoshimachiyuri <39732771+Hoshimachiyuri@users.noreply.github.com>
2020-07-05 15:04:39 +0800

    Add comment for bitrate.ps1

--- utils/bitrate.ps1
+++ utils/bitrate.ps1
@@ -1,4 +1,4 @@
-New-Variable -Name TARGET_BITRATE -Value 200 -Option Constant
+New-Variable -Name TARGET_BITRATE -Value 200 -Option Constant # Target bitrate limit
 $HAVE_HI_BIT=0
 Get-Location | findstr 'utils'
 if ($? -eq "True"){
@@ -17,10 +17,13 @@ $files = Get-Item *.mp3
 foreach ($file in $files) {
     $null = $filesize = Get-ChildItem | ForEach-Object {[math]::ceiling($file.length / 1kb)}
     if ($filesize -gt '51') {
+        # Only check files with size > 50kb
         $info = &"ffprobe" $file.Name 2>&1 | findstr "Duration"
         $BITRATE = ForEach-Object {($info -split "[ ]")[7]}
         if ($BITRATE -gt $TARGET_BITRATE) {
             if ($args[0] -ne "-o") {
+                # Arguments:
+                #   -o: Only output audio filenames
                 Write-Output "Too high bitrate for '$file' : $BITRATE kbps"
                 $HAVE_HI_BIT = $HAVE_HI_BIT + 1
             }

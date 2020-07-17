d5d98227aa8fed0d0bf5afa067916f6da7cdee4d
Hoshimachiyuri <39732771+Hoshimachiyuri@users.noreply.github.com>
2020-07-17 20:16:08 +0800

    Add 17 sounds from pLUf75CZg38 and format bitrate.ps1

--- utils/bitrate.ps1
+++ utils/bitrate.ps1
@@ -1,22 +1,22 @@
 $TARGET_BITRATE = 200 # Target bitrate limit
-$HAVE_HI_BIT=0
-if (Test-Path -Path ..\public\assets){
+$HAVE_HI_BIT = 0
+if (Test-Path -Path ..\public\assets) {
     Set-Location ..\public\assets
 }
-elseif (Test-Path -Path public\assets){
-        Set-Location public\assets
-    }
+elseif (Test-Path -Path public\assets) {
+    Set-Location public\assets
+}
 else {
-        Write-Output "Can't get location. Please switch to root of repository."
+    Write-Output "Can't get location. Please switch to root of repository."
 }
 
 $files = Get-Item *.mp3
 foreach ($file in $files) {
-    $null = $filesize = Get-ChildItem | ForEach-Object {[math]::ceiling($file.length / 1kb)}
+    $null = $filesize = Get-ChildItem | ForEach-Object { [math]::ceiling($file.length / 1kb) }
     if ($filesize -gt '51') {
         # Only check files with size > 50kb
         $info = &"ffprobe" -v error -show_entries format=bit_rate -i $file.Name
-        $BITRATE = ForEach-Object {[math]::ceiling(($info -split "[=]")[2] / 1kb)}
+        $BITRATE = ForEach-Object { [math]::ceiling(($info -split "[=]")[2] / 1kb) }
         if ($BITRATE -gt $TARGET_BITRATE) {
             if ($args[0] -ne "-o") {
                 # Arguments:

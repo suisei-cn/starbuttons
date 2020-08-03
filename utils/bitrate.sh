aba2599ad5e3a270dc9e8d80d9ba70b62b1120cf
Hoshimachiyuri <39732771+Hoshimachiyuri@users.noreply.github.com>
2020-08-04 03:30:26 +0800

    Fix(utils/bitrate.sh):Fix file bitrate calculation

--- utils/bitrate.sh
+++ utils/bitrate.sh
@@ -10,7 +10,7 @@ TARGET_BITRATE=200
 cd public/assets
 HAVE_HI_BIT=0
 for i in $(find . -size +$FILTER_FILE_SIZE | grep -v README.md); do
-    BITRATE=$(($(ffprobe -v error -show_entries format=bit_rate -i $i | sed -n "2p" | awk -F '[=]' '{print $2}') / 1000))
+    BITRATE=$(($(ffprobe -v error -show_entries format=bit_rate -i $i | sed -n "2p" | awk -F '[=]' '{print $2}') / 1024))
     if [ "$BITRATE" -gt "$TARGET_BITRATE" ]; then
         if [ "$1" != "-o" ]; then
             echo "Too high bitrate for '$i' : $BITRATE kbps"

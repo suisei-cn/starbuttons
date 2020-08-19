effeaa3d874dc4bfa38a0e7ee2dfe03b18b2e0d2
Outvi V <git@outv.im>
2020-08-20 05:27:57 +0800

    feat(BaseButton): basic layout

---
+++ src/components/BaseButton.svelte
@@ -0,0 +1,20 @@
+<div class="baseButton" on:click="{playSound}">
+    test
+</div>
+
+<script lang="ts">
+    import type {Sound} from '../types'
+    // Props
+    let item: Sound;
+    let noclickplay: boolean;
+
+    // Methods
+    function playSound() {
+        item.file
+    }
+</script>
+
+<style lang="scss">
+    @import '../styles/BaseButton.scss';
+    @import '../styles/utils.scss';
+</style>

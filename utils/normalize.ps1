50c07eb2e755ea63e039d8ee9b3615fee53efc80
Outvi V <git@outv.im>
2020-08-22 00:42:58 +0800

    feat(utils/normalize): also normalize vollume

--- utils/normalize.ps1
+++ utils/normalize.ps1
@@ -13,7 +13,7 @@ $FILENAMES = &"utils\bitrate.ps1" -o
 Set-Location public\assets
 foreach ($i in $FILENAMES){
     Rename-Item $i -NewName 'current.mp3'
-    &"ffmpeg" -i current.mp3 -ab 192k -map_metadata -1 $i
+    &"ffmpeg" -i current.mp3 -ab 192k -af loudnorm=I=-16:TP=-2:LRA=11 -map_metadata -1 $i
     Remove-Item current.mp3
 }
 Set-Location ..\..
--- utils/normalize.sh
+++ utils/normalize.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 FILENAMES=$(utils/bitrate.sh -o)
 cd public/assets
 for i in $(echo "$FILENAMES" | tr ' ' ' '); do
     mv $i current.mp3
-    ffmpeg -i current.mp3 -ab 192k -map_metadata -1 $i
+    ffmpeg -i current.mp3 -ab 192k -af loudnorm=I=-16:TP=-2:LRA=11 -map_metadata -1 $i
     rm current.mp3
 done
 cd ../..

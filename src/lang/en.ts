e5255b98952ac7ce53c525b64db27a22f5aabea0
Outvi V <git@outv.im>
2020-08-24 23:45:18 +0800

    lint(src/lang):

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -1,31 +1,31 @@
 export default {
-  "Do Not Click Me": "Do Not Click Me",
-  Repeat: "Repeat",
-  "Dark Theme": "Dark Theme",
-  "Light Theme": "Light Theme",
-  GitHub: "GitHub",
-  Telegram: "Telegram",
-  Discord: "Discord",
-  English: "English",
-  "Toggle chorus mode": "Toggle chorus mode",
-  "Music board": "Music board",
-  Back: "Back",
-  "Suisei Remix Editor": "Suisei Remix Editor",
-  experimental: "experimental",
-  Play: "Play",
-  Export: "Export",
-  Clear: "Clear",
-  "Your device screen is too small to run this editor. Sorry~":
-    "Your device screen is too small to run this editor. Sorry~",
-  "long-editor-description":
+  'Do Not Click Me': 'Do Not Click Me',
+  Repeat: 'Repeat',
+  'Dark Theme': 'Dark Theme',
+  'Light Theme': 'Light Theme',
+  GitHub: 'GitHub',
+  Telegram: 'Telegram',
+  Discord: 'Discord',
+  English: 'English',
+  'Toggle chorus mode': 'Toggle chorus mode',
+  'Music board': 'Music board',
+  Back: 'Back',
+  'Suisei Remix Editor': 'Suisei Remix Editor',
+  experimental: 'experimental',
+  Play: 'Play',
+  Export: 'Export',
+  Clear: 'Clear',
+  'Your device screen is too small to run this editor. Sorry~':
+    'Your device screen is too small to run this editor. Sorry~',
+  'long-editor-description':
     "Here you can remix with Suisei's voice clips and export your sparks.<br>The full track length is 5 seconds. Drag the voice clip button on wherever you want on the track.<br>Please comply to <a href='https://en.hololive.tv/terms'>Hololive's fan work guidelines</a> when creating derivative works.<br>(This editor is experimental and may change a lot and add many features in the future.)",
-  "Error in sound playing:": "Error in sound playing:",
+  'Error in sound playing:': 'Error in sound playing:',
   "We've known about it and will work on it soon.":
     "We've known about it and will work on it soon.",
-  "Sound list fetch error:": "Sound list fetch error:",
-  "Voices are still loading. Please be patient...":
-    "Voices are still loading. Please be patient...",
-  "Unrecognized sound list.": "Unrecognized sound list.",
-  "Still loading...": "Still loading...",
-  Uncategorized: "Uncategorized"
-};
+  'Sound list fetch error:': 'Sound list fetch error:',
+  'Voices are still loading. Please be patient...':
+    'Voices are still loading. Please be patient...',
+  'Unrecognized sound list.': 'Unrecognized sound list.',
+  'Still loading...': 'Still loading...',
+  Uncategorized: 'Uncategorized',
+}

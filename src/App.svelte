7630453bf3e70b715ec8f86d6165de17200f67f8
Outvi V <git@outv.im>
2020-08-24 23:30:28 +0800

    feat(App): basic page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,27 @@
 <main>
-  <BaseButton></BaseButton>
+  {#each sounds as sound}
+    <BaseButton item="{sound}" />
+  {/each}
 </main>
 
 <script lang="ts">
-  import BaseButton from "./components/BaseButton.svelte";
+  import { onMount } from 'svelte'
+  import BaseButton from './components/BaseButton.svelte'
+  import type { SiteConfig, Sound } from './types'
+
+  export let siteConfig: SiteConfig
+  let sounds: Sound[] = []
+
+  onMount(async () => {
+    sounds = await fetch(siteConfig.sounds)
+      .then((x) => x.json())
+      .catch(() => {
+        console.error('Failed to fetch sounds')
+        return []
+      })
+  })
 </script>
 
 <style lang="scss">
+
 </style>

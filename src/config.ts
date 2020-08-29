7d0bf31063b70616e1acb2eb7a33ab0d803a077f
Outvi V <git@outv.im>
2020-08-29 11:40:31 +0800

    chore(prod): use CloudFront CDN

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig } from './types'
 
 export default {
-  sounds: 'https://suisei-cn.github.io/sbtn-assets/sounds.json',
-  categories: 'https://suisei-cn.github.io/sbtn-assets/categories.json',
-  assets_path: 'https://suisei-cn.github.io/sbtn-assets/assets/',
+  sounds: 'https://didjcksqhgjn7.cloudfront.net/sounds.json',
+  categories: 'https://didjcksqhgjn7.cloudfront.net/categories.json',
+  assets_path: 'https://didjcksqhgjn7.cloudfront.net/assets/',
 } as SiteConfig

6cd947601afc3b3c212da598ad1112559ceb23fe
Outvi V <git@outv.im>
2020-09-23 13:26:44 +0800

    chore: optimized Sentry build

--- utils/sentry-commit.sh
+++ utils/sentry-commit.sh
@@ -2,5 +2,5 @@
 set -e
 
 REV=$(node_modules/.bin/sentry-cli releases propose-version)
-node_modules/.bin/sentry-cli releases set-commits --auto $GITHUB_SHA
-node_modules/.bin/sentry-cli releases deploys $GITHUB_SHA new -e PROD
+node_modules/.bin/sentry-cli releases set-commits --auto $REV
+node_modules/.bin/sentry-cli releases deploys $REV new -e PROD

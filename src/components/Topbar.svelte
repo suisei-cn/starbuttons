ea7d0a156f12d056a134da7f0e9c51d54fc8e838
Outvi V <git@outv.im>
2020-10-01 19:16:07 +0800

    feat: improved UI on Topbar

--- src/components/Topbar.svelte
+++ src/components/Topbar.svelte
@@ -1,26 +1,48 @@
-<header>
-  {$_('Suisei Channel reaches 500k subscribers!')}
-  <a
-    href="https://twitter.com/suisei_hosimati/status/1310208318338510850"
-    target="_blank"
-  >
-    {$_('3rd YouTube Live on Oct 19')}
-  </a>
-</header>
+{#if display}
+  <header>
+    <span>
+      {$_('Suisei Channel reaches 500k subscribers!')}
+      <a
+        href="https://twitter.com/suisei_hosimati/status/1310208318338510850"
+        target="_blank"
+      >
+        {$_('3rd YouTube Live on Oct 19')}
+      </a>
+    </span>
+    <svg
+      id="close"
+      on:click="{(e) => (display = false)}"
+      viewBox="0 0 4 4"
+      stroke="white"
+      stroke-width="0.25"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      fill="none"
+    >
+      <path d="M3 1L1 3"></path>
+      <path d="M1 1l2 2"></path>
+    </svg>
+  </header>
+{/if}
 
 <script lang="ts">
   import { _ } from 'svelte-i18n'
+  let display = true
 </script>
 
 <style lang="scss">
+  $header-height: 40px;
   header {
     position: absolute;
-    height: 30px;
-    line-height: 30px;
+    height: $header-height;
+    line-height: $header-height;
     width: 100vw;
     text-align: center;
     background: #000;
     z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     a {
       color: aqua;
@@ -31,6 +53,13 @@
         text-decoration: underline;
       }
     }
+  }
+
+  #close {
+    position: absolute;
+    right: 20px;
+    height: $header-height * 0.6;
+    cursor: pointer;
   }
 
   @media (max-width: 750px) {

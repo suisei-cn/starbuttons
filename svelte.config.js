a410a3c9b8e0ee738b1238a895e475d64bb013b1
Outvi V <git@outv.im>
2020-10-26 18:26:45 +0800

    chore: switch to Snowpack [1/inf]

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
 const sveltePreprocess = require('svelte-preprocess')
 
 module.exports = {
-  preprocess: sveltePreprocess(),
+  preprocess: sveltePreprocess({
+    defaults: {
+      script: 'typescript',
+    },
+  }),
 }

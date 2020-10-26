ee28c602cf712d7e0e5ca07849180661e8a32b17
Outvi V <git@outv.im>
2020-10-26 18:26:45 +0800

    chore: switch to Snowpack [1/inf]

---
+++ snowpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-sass',
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'svelte-check --output human',
+        watch: '$1 --watch',
+        output: 'stream',
+      },
+    ],
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {},
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+}

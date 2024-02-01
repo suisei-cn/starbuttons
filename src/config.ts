1c048486fd339be71c985424dcefb10714316f2a
Outvi V <19144373+outloudvi@users.noreply.github.com>
2024-02-01 10:15:13 +0800

    chore: update endpoint S3

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { SiteConfig } from './types'
 
 export default {
-  sounds: 'https://sbtn-assets.outv.im/sounds.json',
-  categories: 'https://sbtn-assets.outv.im/categories.json',
-  assets_path: 'https://sbtn-assets.outv.im/assets/',
-  notice: 'https://sbtn-assets.outv.im/notice.json',
+  sounds: 'https://io.cafe.two.li/suisei-moe/sounds.json',
+  categories: 'https://io.cafe.two.li/suisei-moe/categories.json',
+  assets_path: 'https://io.cafe.two.li/suisei-moe/assets/',
+  notice: 'https://io.cafe.two.li/suisei-moe/notice.json',
 } as SiteConfig
 
 export const testingConfig = {

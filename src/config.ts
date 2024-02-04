d6365243070db27293c0a67c82601ff89141e111
Outvi V <19144373+outloudvi@users.noreply.github.com>
2024-02-05 00:31:26 +0800

    chore(config): drop S3

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { SiteConfig } from './types'
 
 export default {
-  sounds: 'https://io.cafe.two.li/suisei-moe/sounds.json',
-  categories: 'https://io.cafe.two.li/suisei-moe/categories.json',
-  assets_path: 'https://io.cafe.two.li/suisei-moe/assets/',
-  notice: 'https://io.cafe.two.li/suisei-moe/notice.json',
+  sounds: 'https://suisei-cn.github.io/sbtn-assets/sounds.json',
+  categories: 'https://suisei-cn.github.io/sbtn-assets/categories.json',
+  assets_path: 'https://suisei-cn.github.io/sbtn-assets/assets/',
+  notice: 'https://suisei-cn.github.io/sbtn-assets/notice.json',
 } as SiteConfig
 
 export const testingConfig = {

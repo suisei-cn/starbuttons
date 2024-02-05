b253b8bb9d5d63578ef21c6164a645c260f96708
Outvi V <19144373+outloudvi@users.noreply.github.com>
2024-02-05 11:50:15 +0000

    chore: offboard Sentry

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import App from './App.svelte'
 import config, { testingConfig } from './config'
 import initi18n from './utils/i18n'
-import * as Sentry from '@sentry/browser'
 
 initi18n()
-
-Sentry.init({
-  dsn:
-    'https://fea8a42155df4851b95de0a8de86cb17@o407537.ingest.sentry.io/5276695',
-  release: 'NULL_RELEASE',
-})
 
 const app = new App({
   target: document.body,
--- utils/sentry-commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-REV=$(node_modules/.bin/sentry-cli releases propose-version)
-node_modules/.bin/sentry-cli releases set-commits --auto $REV
-node_modules/.bin/sentry-cli releases deploys $REV new -e PROD
